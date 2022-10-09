c6d5d2f78c18416a0895e6564476abbf144094c5
ikersuarez01 <ikersuarez01@gmail.com>
2022-10-09 18:36:11 +0200

    Project

---
+++ AliensBorrachos/Assets/Scripts/Canvas/GoToKitchen.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GoToKitchen : MonoBehaviour
+{
+    [SerializeField] GameObject mainButton;
+    [SerializeField] GameObject kitchenButton;
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject kitchenCamera;
+    private void Start()
+    {
+        kitchenButton.GetComponent<Button>().onClick.AddListener(onClick);
+    }
+    private void onClick()
+    {
+        mainButton.SetActive(true);
+        kitchenButton.SetActive(false);
+
+        mainCamera.SetActive(false);
+        kitchenCamera.SetActive(true);
+    }
+}
